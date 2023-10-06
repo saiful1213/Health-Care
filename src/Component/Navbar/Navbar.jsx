23b7d63dc8ab7162bae9732c3117c8545ce92ba5
saiful-islam <md2215566@gmail.com>
2023-10-07 02:50:19 +0600

    navbar fixed done

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
    </>
 
    return (
-      <div className="bg-base-100 shadow-md">
+      <div className="shadow-md w-full bg-base-100 fixed top-0">
          <div className="navbar max-w-7xl mx-auto">
             <div className="navbar-start">
                <div className="dropdown">
